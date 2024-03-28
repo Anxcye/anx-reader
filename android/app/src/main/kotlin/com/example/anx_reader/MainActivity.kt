af3769b96bd999a4a4a436539fb9e198785edab7
Anxcye <axyyu@outlook.com>
2024-03-28 19:26:58 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/anx_reader/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.anx_reader
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/page/Home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
