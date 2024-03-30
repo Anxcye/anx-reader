7b20f906130105b8765916d576533f90d085c8d7
Anxcye <axyyu@outlook.com>
2024-03-30 17:26:24 +0800

    feat: add 4 pages

---
+++ lib/page/notes_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NotesPage extends StatelessWidget {
+  const NotesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
