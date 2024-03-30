9b45d64f86fd3296e22c36870d38d9e787c797aa
Anxcye <anxcye@outlook.com>
2024-03-30 15:50:26 +0800

    feat: better grid

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -51,12 +51,13 @@ class _HomePageState extends State<HomePage> {
 
   Widget _bookList() {
     return GridView.builder(
+      padding: const EdgeInsets.fromLTRB(20, 20, 20, 10),
       itemCount: _books.length,
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 3,
         childAspectRatio: 0.6,
-        mainAxisSpacing: 10,
-        crossAxisSpacing: 10,
+        mainAxisSpacing: 30,
+        crossAxisSpacing: 20,
       ),
       itemBuilder: (BuildContext context, int index) {
         Book book = _books[index];
