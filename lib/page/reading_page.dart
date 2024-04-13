2949c33f289aa2fd01b193f87c7d01bff8386406
Anxcye <axyyu@outlook.com>
2024-04-13 19:16:22 +0800

    feat: change bottom app bar color

--- lib/page/reading_page.dart
+++ lib/page/reading_page.dart
@@ -6,6 +6,8 @@ import 'package:anx_reader/page/book_player/epub_player.dart';
 import 'package:epubx/epubx.dart';
 import 'package:flutter/material.dart';
 
+import '../utils/generate_index_html.dart';
+
 class ReadingPage extends StatefulWidget {
   final Book book;
 
@@ -17,27 +19,49 @@ class ReadingPage extends StatefulWidget {
 
 class _ReadingPageState extends State<ReadingPage> {
   late Book _book;
-  EpubContent? _content;
+  String? _content;
+  BookStyle _bookStyle = BookStyle();
+  bool _isAppBarVisible = false;
+  double _appBarTopPosition = -kToolbarHeight;
+  double readProgress = 0.0;
   final _epubPlayerKey = GlobalKey<EpubPlayerState>();
 
   @override
   void initState() {
     super.initState();
     _book = widget.book;
-    initializeContent();
+    loadContent();
   }
 
-  Future<void> initializeContent() async {
-    EpubBookRef epubBookRef =
-        await EpubReader.openBook(File(_book.filePath).readAsBytes());
-    _content = await EpubReader.readContent(epubBookRef.Content!);
-    setState(() {});
+  void loadContent() {
+    var content = generateIndexHtml(
+        widget.book, _bookStyle, widget.book.lastReadPosition);
+    setState(() {
+      _content = content;
+    });
+  }
+
+  void showOrHideAppBarAndBottomBar() {
+    setState(() {
+      if (_isAppBarVisible) {
+        _appBarTopPosition = -kToolbarHeight;
+      } else {
+        _appBarTopPosition = 0;
+      }
+      _isAppBarVisible = !_isAppBarVisible;
+    });
+  }
+
+  void onReadProgressChanged(double value) {
+    setState(() {
+      readProgress = value;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     if (_content == null) {
-      return CircularProgressIndicator();
+      return const Center(child: CircularProgressIndicator());
     } else {
       return PopScope(
         canPop: false,
@@ -47,13 +71,81 @@ class _ReadingPageState extends State<ReadingPage> {
           Navigator.pop(context, cfi);
         },
         child: Scaffold(
-          appBar: AppBar(
-            title: Text(_book.title),
-          ),
-          body: EpubPlayer(
-            key: _epubPlayerKey,
-            book: _book,
-            style: BookStyle(),
+          body: Stack(
+            children: [
+              EpubPlayer(
+                key: _epubPlayerKey,
+                content: _content!,
+                showOrHideAppBarAndBottomBar: showOrHideAppBarAndBottomBar,
+              ),
+              if (_isAppBarVisible)
+                AnimatedPositioned(
+                  top: _appBarTopPosition,
+                  left: 0,
+                  right: 0,
+                  duration: const Duration(milliseconds: 3000),
+                  child: AppBar(
+                    title: Text(_book.title),
+                  ),
+                ),
+              if (_isAppBarVisible)
+                AnimatedPositioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  duration: const Duration(milliseconds: 3000),
+                  // child: BottomAppBar(
+                  child: Container(
+                    color: Theme.of(context).colorScheme.surfaceVariant,
+                    child: Wrap(
+                      children: [
+                        Container(
+                          height: 700,
+                          child: ListView(
+
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            IconButton(
+                              icon: const Icon(Icons.toc),
+                              onPressed: () {
+                                // _epubPlayerKey.currentState!.previousPage();
+                              },
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.edit_note),
+                              onPressed: () {
+                                // _epubPlayerKey.currentState!.nextPage();
+                              },
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.data_usage),
+                              onPressed: () {
+                                // _epubPlayerKey.currentState!.nextPage();
+                              },
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.color_lens),
+                              onPressed: () {
+                                // _epubPlayerKey.currentState!.nextPage();
+                              },
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.text_fields),
+                              onPressed: () {
+                                // _epubPlayerKey.currentState!.nextPage();
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+            ],
           ),
         ),
       );
