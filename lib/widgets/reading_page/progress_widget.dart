18bd0c3c85fcecfda18b593324627c961a86c00c
Anxcye <axyyu@outlook.com>
2024-04-14 14:53:28 +0800

    factor: move two widget to a own file

---
+++ lib/widgets/reading_page/progress_widget.dart
@@ -0,0 +1,150 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import '../../page/book_player/epub_player.dart';
+
+class ProgressWidget extends StatefulWidget {
+  final GlobalKey<EpubPlayerState> epubPlayerKey;
+  final Function(bool) showOrHideAppBarAndBottomBar;
+  final double readProgress;
+
+  const ProgressWidget({
+    super.key,
+    required this.epubPlayerKey,
+    required this.showOrHideAppBarAndBottomBar,
+    required this.readProgress,
+  });
+
+  @override
+  State<ProgressWidget> createState() => _ProgressWidgetState();
+}
+
+class _ProgressWidgetState extends State<ProgressWidget> {
+  Timer? _sliderTimer;
+  double _readProgress = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _readProgress = widget.readProgress;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          const SizedBox(height: 10),
+          Text(
+            widget.epubPlayerKey.currentState!.chapterTitle,
+            style: const TextStyle(
+              fontSize: 20,
+              fontFamily: 'SourceHanSerif',
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Divider(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: () {
+                  widget.epubPlayerKey.currentState!.prevChapter();
+                  widget.showOrHideAppBarAndBottomBar(false);
+                },
+              ),
+              Expanded(
+                child: Slider(
+                  inactiveColor: Colors.grey.shade300,
+                  value: _readProgress,
+                  onChanged: (value) {
+                    _sliderTimer?.cancel();
+                    _sliderTimer = Timer(
+                      const Duration(milliseconds: 300),
+                      () {
+                        widget.epubPlayerKey.currentState!
+                            .goToPersentage(value);
+                      },
+                    );
+                  },
+                ),
+              ),
+              IconButton(
+                icon: const Icon(Icons.arrow_forward),
+                onPressed: () {
+                  widget.epubPlayerKey.currentState!.nextChapter();
+                  widget.showOrHideAppBarAndBottomBar(false);
+                },
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              ProgressDisplayer(
+                mainText: widget.epubPlayerKey.currentState!.chapterCurrentPage
+                    .toString(),
+                subText: 'current page',
+              ),
+              ProgressDisplayer(
+                mainText: widget.epubPlayerKey.currentState!.chapterTotalPage
+                    .toString(),
+                subText: 'total page',
+              ),
+              ProgressDisplayer(
+                mainText: widget.epubPlayerKey.currentState!.progress
+                    .toStringAsFixed(2),
+                subText: 'percentage',
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          const Divider(),
+        ],
+      ),
+    );
+  }
+}
+
+class ProgressDisplayer extends StatelessWidget {
+  const ProgressDisplayer({
+    super.key,
+    required this.mainText,
+    required this.subText,
+  });
+
+  final mainText;
+  final subText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        // width: 50,
+        child: Column(
+          children: [
+            Text(
+              mainText,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'SourceHanSerif',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              subText,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 10,
+                fontFamily: 'SourceHanSerif',
+                fontWeight: FontWeight.w300,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
