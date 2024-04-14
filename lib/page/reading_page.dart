4d5b3377b1a287b13492b1adf8c30600cc90f391
Anxcye <anxcye@outlook.com>
2024-04-14 14:14:13 +0800

    feat: progress command

--- lib/page/reading_page.dart
+++ lib/page/reading_page.dart
@@ -1,11 +1,12 @@
-import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
 
 import 'package:anx_reader/models/book.dart';
 import 'package:anx_reader/models/book_style.dart';
 import 'package:anx_reader/page/book_player/epub_player.dart';
-import 'package:epubx/epubx.dart';
 import 'package:flutter/material.dart';
 
+import '../models/toc_item.dart';
 import '../utils/generate_index_html.dart';
 
 class ReadingPage extends StatefulWidget {
@@ -24,6 +25,9 @@ class _ReadingPageState extends State<ReadingPage> {
   bool _isAppBarVisible = false;
   double _appBarTopPosition = -kToolbarHeight;
   double readProgress = 0.0;
+  List<TocItem> _tocItems = [];
+  Widget _currentPage = const SizedBox(height: 1);
+  Timer? _sliderTimer;
   final _epubPlayerKey = GlobalKey<EpubPlayerState>();
 
   @override
@@ -41,20 +45,53 @@ class _ReadingPageState extends State<ReadingPage> {
     });
   }
 
-  void showOrHideAppBarAndBottomBar() {
+  void showOrHideAppBarAndBottomBar(bool show) {
     setState(() {
-      if (_isAppBarVisible) {
-        _appBarTopPosition = -kToolbarHeight;
-      } else {
+      if (show) {
+        if (_isAppBarVisible) {
+          show = false;
+        }
         _appBarTopPosition = 0;
+      } else {
+        _appBarTopPosition = -kToolbarHeight;
       }
-      _isAppBarVisible = !_isAppBarVisible;
+      _isAppBarVisible = show;
     });
   }
 
   void onReadProgressChanged(double value) {
     setState(() {
       readProgress = value;
+    });
+  }
+
+  Future<void> tocHandler() async {
+    String toc = await _epubPlayerKey.currentState!.getToc();
+    setState(() {
+      _tocItems =
+          (json.decode(toc) as List).map((i) => TocItem.fromJson(i)).toList();
+      _currentPage = TocWidget(
+          tocItems: _tocItems,
+          epubPlayerKey: _epubPlayerKey,
+          hideAppBarAndBottomBar: showOrHideAppBarAndBottomBar);
+    });
+  }
+
+  void noteHandler() {
+    setState(() {
+      _currentPage = Container(
+        height: 700,
+      );
+    });
+  }
+
+  Future<void> progressHandler() async {
+    readProgress = _epubPlayerKey.currentState!.progress;
+    print(readProgress);
+    setState(() {
+      _currentPage = Container(
+        height: 700,
+      );
     });
   }
 
@@ -99,12 +136,61 @@ class _ReadingPageState extends State<ReadingPage> {
                     color: Theme.of(context).colorScheme.surfaceVariant,
                     child: Wrap(
                       children: [
+                        // TocWidget(tocItems: tocItems),
+                        // _currentPage,
                         Container(
-                          height: 700,
-                          child: ListView(
-
-                          ),
-                        ),
+                            height: 300,
+                            child: Column(
+                              children: [
+                                SizedBox(height: 10),
+                                Text(
+                                  _epubPlayerKey.currentState!.chapterTitle,
+                                  style: const TextStyle(
+                                    fontSize: 20,
+                                    fontFamily: 'SourceHanSerif',
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Divider(),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  children: [
+                                    IconButton(
+                                      icon: const Icon(Icons.arrow_back),
+                                      onPressed: () {
+                                        _epubPlayerKey.currentState!
+                                            .prevChapter();
+                                      },
+                                    ),
+                                    Expanded(
+                                      child: Slider(
+                                          inactiveColor: Colors.grey.shade300,
+                                          value: readProgress,
+                                          onChanged: (value) {
+                                            setState(() {
+                                              readProgress = value;
+                                            });
+                                            _sliderTimer?.cancel();
+                                            _sliderTimer = Timer(
+                                                const Duration(
+                                                    milliseconds: 300), () {
+                                              _epubPlayerKey.currentState!
+                                                  .goToPersentage(value);
+                                            });
+                                          }),
+                                    ),
+                                    IconButton(
+                                      icon: const Icon(Icons.arrow_forward),
+                                      onPressed: () {
+                                        _epubPlayerKey.currentState!
+                                            .nextChapter();
+                                      },
+                                    ),
+                                  ],
+                                )
+                              ],
+                            )),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceAround,
                           crossAxisAlignment: CrossAxisAlignment.center,
@@ -112,19 +198,19 @@ class _ReadingPageState extends State<ReadingPage> {
                             IconButton(
                               icon: const Icon(Icons.toc),
                               onPressed: () {
-                                // _epubPlayerKey.currentState!.previousPage();
+                                tocHandler();
                               },
                             ),
                             IconButton(
                               icon: const Icon(Icons.edit_note),
                               onPressed: () {
-                                // _epubPlayerKey.currentState!.nextPage();
+                                noteHandler();
                               },
                             ),
                             IconButton(
                               icon: const Icon(Icons.data_usage),
                               onPressed: () {
-                                // _epubPlayerKey.currentState!.nextPage();
+                                progressHandler();
                               },
                             ),
                             IconButton(
@@ -150,5 +236,36 @@ class _ReadingPageState extends State<ReadingPage> {
         ),
       );
     }
+  }
+}
+
+class TocWidget extends StatelessWidget {
+  const TocWidget({
+    super.key,
+    required this.tocItems,
+    required this.epubPlayerKey,
+    required this.hideAppBarAndBottomBar,
+  });
+
+  final List<TocItem> tocItems;
+  final epubPlayerKey;
+  final hideAppBarAndBottomBar;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 700,
+        child: ListView.builder(
+          itemCount: tocItems.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(tocItems[index].label),
+              onTap: () {
+                hideAppBarAndBottomBar(false);
+                epubPlayerKey.currentState!.goTo(tocItems[index].href);
+              },
+            );
+          },
+        ));
   }
 }
