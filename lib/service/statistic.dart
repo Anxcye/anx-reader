4c13a3f25c6f9fe67bf0928bfca15b8485507f30
Anxcye <axyyu@outlook.com>
2024-04-16 14:20:20 +0800

    feat: change style of stastic page

---
+++ lib/service/statistic.dart
@@ -0,0 +1,25 @@
+import 'package:anx_reader/models/reading_time.dart';
+
+import '../dao/reading_time.dart';
+
+class StatisticService {
+  static StatisticService? _instance;
+
+  StatisticService._() {
+    Future.microtask(() async {
+      readingTimes = await selectAllReadingTime();
+    });
+  }
+
+  factory StatisticService() {
+    _instance ??= StatisticService._();
+    return _instance!;
+  }
+
+  List<ReadingTime> readingTimes = [];
+
+  int get totalReadingTime {
+    return readingTimes.fold<int>(0, (previousValue, element) => previousValue + element.readingTime);
+  }
+
+}
