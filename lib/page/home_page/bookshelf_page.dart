1252fddcc47712ce5fe6c228c3275b8183b62eb1
Anxcye <anxcye@outlook.com>
2024-05-09 14:53:12 +0800

    feat: add a sync button

--- lib/page/home_page/bookshelf_page.dart
+++ lib/page/home_page/bookshelf_page.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:anx_reader/l10n/localization_extension.dart';
+import 'package:anx_reader/utils/webdav/common.dart';
 import 'package:anx_reader/widgets/book_list.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
@@ -17,12 +18,18 @@ class BookshelfPage extends StatefulWidget {
   State<BookshelfPage> createState() => _BookshelfPageState();
 }
 
-class _BookshelfPageState extends State<BookshelfPage> {
+class _BookshelfPageState extends State<BookshelfPage>
+    with SingleTickerProviderStateMixin {
   List<Book> _books = [];
+  AnimationController? _syncAnimationController;
 
   @override
   void initState() {
     super.initState();
+    _syncAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    )..repeat();
     _refreshBookList();
   }
 
@@ -51,12 +58,38 @@ class _BookshelfPageState extends State<BookshelfPage> {
     _refreshBookList();
   }
 
+  Widget syncButton() {
+    return StreamBuilder<bool>(
+      stream: AnxWebdav.syncing,
+      builder: (context, snapshot) {
+        if (snapshot.hasData && snapshot.data == true) {
+          _syncAnimationController?.repeat();
+          return IconButton(
+            icon: RotationTransition(
+              turns: Tween(begin: 1.0, end: 0.0).animate(_syncAnimationController!),
+              child: const Icon(Icons.sync),
+            ),
+            onPressed: null,
+          );
+        } else {
+          return IconButton(
+            icon: const Icon(Icons.sync),
+            onPressed: () {
+              AnxWebdav.syncData(SyncDirection.both);
+            },
+          );
+        }
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(context.appName),
         actions: [
+          syncButton(),
           IconButton(
             icon: const Icon(Icons.add),
             onPressed: _importBook,
