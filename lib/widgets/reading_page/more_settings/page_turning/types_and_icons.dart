3d2dbe9d4bdfc310de7a49c413659d70e863d84d
Anxcye <anxcye@outlook.com>
2024-07-11 19:14:24 +0800

    feat: page turning control

---
+++ lib/widgets/reading_page/more_settings/page_turning/types_and_icons.dart
@@ -0,0 +1,56 @@
+import 'package:anx_reader/widgets/reading_page/more_settings/page_turning/diagram.dart';
+
+List<PageTurningType> type1 = [
+  PageTurningType.prev,
+  PageTurningType.menu,
+  PageTurningType.next,
+  PageTurningType.prev,
+  PageTurningType.menu,
+  PageTurningType.next,
+  PageTurningType.prev,
+  PageTurningType.menu,
+  PageTurningType.next
+];
+List<int> icon1 = [5, 3, 4];
+
+List<PageTurningType> type2 = [
+  PageTurningType.prev,
+  PageTurningType.prev,
+  PageTurningType.next,
+  PageTurningType.prev,
+  PageTurningType.menu,
+  PageTurningType.next,
+  PageTurningType.prev,
+  PageTurningType.next,
+  PageTurningType.next
+];
+List<int> icon2 = [5, 3, 4];
+
+List<PageTurningType> type3 = [
+  PageTurningType.prev,
+  PageTurningType.prev,
+  PageTurningType.next,
+  PageTurningType.prev,
+  PageTurningType.menu,
+  PageTurningType.next,
+  PageTurningType.next,
+  PageTurningType.next,
+  PageTurningType.next
+];
+List<int> icon3 = [5, 3, 4];
+
+List<PageTurningType> type4 = [
+  PageTurningType.prev,
+  PageTurningType.menu,
+  PageTurningType.prev,
+  PageTurningType.prev,
+  PageTurningType.menu,
+  PageTurningType.next,
+  PageTurningType.next,
+  PageTurningType.next,
+  PageTurningType.next
+];
+List<int> icon4 = [5, 3, 4];
+
+List<List<PageTurningType>> pageTurningTypes = [type1, type2, type3, type4];
+List<List<int>> pageTurningIcons = [icon1, icon2, icon3, icon4];
