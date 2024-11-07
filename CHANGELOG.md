868722f21b22d977cfa13fec062fc200675ee0ff
Anxcye <axyyu@outlook.com>
2024-11-07 19:35:23 +0800

    feat: Webview version check

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,53 +1,60 @@
 # Changelog
 
-## Unreleased
-* Feat: Translate selected content
-* Feat: Note add time
-* UI: Optimized the statistic card
-* Fix: Context menu cannot be closed once
+## Todo
+- 选择时移动
+- 简繁转换
+- 自动背景色
 
-* 新增：翻译选中内容
-* 新增：显示笔记添加时间
-* UI：优化了统计卡片
-* 修复：上下文菜单不能一次关闭
+## Unreleased
+- Feat: Translate selected content
+- Feat: Note add time
+- Feat: Webview version check
+- UI: Optimized the statistic card
+- Fix: Context menu cannot be closed once
+
+- 新增：翻译选中内容
+- 新增：显示笔记添加时间
+- 新增：Webview版本检查
+- UI：优化了统计卡片
+- 修复：上下文菜单不能一次关闭
 
 ## 1.1.8 2024-10-23
 
-* Added: Modify import/export file structure
-* Fixed: Book font size cannot maintain relative relationship
-* Fixed: Can be used in lower webview versions (about 92.0.0.0 and above)
+- Added: Modify import/export file structure
+- Fixed: Book font size cannot maintain relative relationship
+- Fixed: Can be used in lower webview versions (about 92.0.0.0 and above)
 
-* 修改：修改了导入导出的文件结构
-* 修复：书籍字体大小不能保持相对关系
-* 修复：能够在较低的 webview 版本中使用(约92.0.0.0及以上)
+- 修改：修改了导入导出的文件结构
+- 修复：书籍字体大小不能保持相对关系
+- 修复：能够在较低的 webview 版本中使用(约92.0.0.0及以上)
 
 Windows version is coming soon!
 Windows端即将发布，敬请期待！
 
 ## 1.1.7 2024-09-11
-* Backup: Export/import all data
-* Ability to click and view large images
-* Convenient back arrow after navigation
-* Multiple pop-up annotations within a pop-up annotation
-* Customizable text indentation size
-* Text selection within pop-up annotations
-* Optimization of status bar and navigation key areas to avoid obstruction by navigation keys
-* Fixed white screen issue when opening files
-* Fixed issue with importing font files with Chinese filenames
-* Shortened TTS reading intervals, especially when using TTS-Server
+- Backup: Export/import all data
+- Ability to click and view large images
+- Convenient back arrow after navigation
+- Multiple pop-up annotations within a pop-up annotation
+- Customizable text indentation size
+- Text selection within pop-up annotations
+- Optimization of status bar and navigation key areas to avoid obstruction by navigation keys
+- Fixed white screen issue when opening files
+- Fixed issue with importing font files with Chinese filenames
+- Shortened TTS reading intervals, especially when using TTS-Server
 
-* 备份：导出/导入全部数据
-* 能够点击查看大图了
-* 跳转后能够有方便地返回箭头
-* 弹注中多次弹注
-* 弹注字体跟随设置
-* 自定义文本缩进大小
-* 弹注中选择文字
-* 状态栏和导航键区域优化，避免了被导航键遮盖
-* 修复打开文件白屏
-* 修复字体文件中中文文件名无法导入
-*  缩短了TTS朗读间隔，尤其是使用TTS-Server时
-* 根据弹注内容调整弹注框大小
+- 备份：导出/导入全部数据
+- 能够点击查看大图了
+- 跳转后能够有方便地返回箭头
+- 弹注中多次弹注
+- 弹注字体跟随设置
+- 自定义文本缩进大小
+- 弹注中选择文字
+- 状态栏和导航键区域优化，避免了被导航键遮盖
+- 修复打开文件白屏
+- 修复字体文件中中文文件名无法导入
+-  缩短了TTS朗读间隔，尤其是使用TTS-Server时
+- 根据弹注内容调整弹注框大小
 
 
 ## 1.1.6 2024-09-03
--- lib/utils/webView/webview_initial_variable.dart
+++ lib/utils/webView/webview_initial_variable.dart
@@ -1,8 +1,11 @@
 import 'package:anx_reader/config/shared_preference_provider.dart';
+import 'package:anx_reader/l10n/generated/L10n.dart';
 import 'package:anx_reader/models/book_style.dart';
 import 'package:anx_reader/models/read_theme.dart';
 import 'package:anx_reader/utils/js/convert_dart_color_to_js.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 
 void webviewInitialVariable(
   InAppWebViewController controller,
@@ -24,8 +27,15 @@ void webviewInitialVariable(
   backgroundColor ??= convertDartColorToJs(readTheme.backgroundColor);
   importing ??= false;
 
+  const minWebviewVersion = 92;
+
   final script = '''
      console.log(navigator.userAgent)
+     const webviewVersion = navigator.userAgent.match(/Chrome\\/(\\d+)/)?.[1]
+     console.log('webviewVersion', webviewVersion)
+     if (webviewVersion && webviewVersion < $minWebviewVersion || !webviewVersion) {
+       window.flutter_inappwebview.callHandler('webviewVersion', webviewVersion)
+     }
      const importing = $importing
      const url = '$url'
      let initialCfi = '$cfi'
@@ -51,6 +61,42 @@ void webviewInitialVariable(
       handlerName: 'webviewInitialVariable',
       callback: (args) async {
         await controller.evaluateJavascript(source: script);
+        return null;
+      });
+  controller.addJavaScriptHandler(
+      handlerName: 'webviewVersion',
+      callback: (args) async {
+        SmartDialog.show(
+          animationType: SmartAnimationType.fade,
+          builder: (context) {
+            final version = args[0];
+            return AlertDialog(
+              title: const Center(
+                child: Icon(Icons.warning_rounded),
+              ),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(L10n.of(context).webview_unsupported_version,
+                      style: Theme.of(context).textTheme.titleMedium),
+                  Text(L10n.of(context)
+                      .webview_unsupported_message(minWebviewVersion, version)),
+                ],
+              ),
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      SmartDialog.dismiss();
+                    },
+                    child: Text(L10n.of(context).webview_cancel)),
+                // TextButton(
+                //     onPressed: () {
+                //     },
+                //     child: Text(L10n.of(context).webview_update))
+              ],
+            );
+          },
+        );
         return null;
       });
 }
