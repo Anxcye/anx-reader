43b203ff5345404484adb21bbf57f32097cdd378
Anxcye <anxcye@outlook.com>
2024-11-27 21:17:23 +0800

    i18n: i18n for column count

--- lib/widgets/reading_page/more_settings/reading_settings.dart
+++ lib/widgets/reading_page/more_settings/reading_settings.dart
@@ -3,6 +3,7 @@ import 'package:anx_reader/enums/convert_chinese_mode.dart';
 import 'package:anx_reader/l10n/generated/L10n.dart';
 import 'package:anx_reader/page/reading_page.dart';
 import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
 
 Widget readingSettings = StatefulBuilder(
   builder: (context, setState) => SingleChildScrollView(
@@ -10,8 +11,9 @@ Widget readingSettings = StatefulBuilder(
       padding: const EdgeInsets.all(18.0),
       child: Column(
         children: [
-          convertChinese(),
           columnCount(),
+          const Divider(height: 18),
+          convertChinese(),
         ],
       ),
     ),
@@ -76,30 +78,47 @@ Widget convertChinese() {
 
 Widget columnCount() {
   return StatefulBuilder(
-    builder: (context, setState) => SegmentedButton(
-      segments: [
-        ButtonSegment<int>(
-          label: Text("auto"),
-          value: 0,
-        ),
-        ButtonSegment<int>(
-          label: Text("single"),
-          value: 1,
-        ),
-        ButtonSegment<int>(
-          label: Text("double"),
-          value: 2,
+    builder: (context, setState) => Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(L10n.of(context).reading_page_column_count,
+            style: Theme.of(context).textTheme.titleMedium),
+        Row(
+          children: [
+            Expanded(
+              child: SegmentedButton(
+                segments: [
+                  ButtonSegment<int>(
+                    label: Text(L10n.of(context).reading_page_auto),
+                    value: 0,
+                    icon: const Icon(Icons.auto_awesome),
+                  ),
+                  ButtonSegment<int>(
+                    label: Text(L10n.of(context).reading_page_single),
+                    value: 1,
+                    icon: const Icon(EvaIcons.book),
+                  ),
+                  ButtonSegment<int>(
+                    label: Text(L10n.of(context).reading_page_double),
+                    value: 2,
+                    icon: const Icon(EvaIcons.book_open),
+                  ),
+                ],
+                selected: {Prefs().bookStyle.maxColumnCount},
+                onSelectionChanged: (value) {
+                  setState(() {
+                    final newBookStyle =
+                        Prefs().bookStyle.copyWith(maxColumnCount: value.first);
+                    Prefs().saveBookStyleToPrefs(newBookStyle);
+                    epubPlayerKey.currentState!.changeStyle(newBookStyle);
+                  });
+                },
+              ),
+            ),
+          ],
         ),
       ],
-      selected: {Prefs().bookStyle.maxColumnCount},
-      onSelectionChanged: (value) {
-        setState(() {
-          Prefs().saveBookStyleToPrefs(
-              Prefs().bookStyle.copyWith(maxColumnCount: value.first));
-          epubPlayerKey.currentState!
-              .changeStyle(Prefs().bookStyle.copyWith(maxColumnCount: value.first));
-        });
-      },
     ),
   );
 }
