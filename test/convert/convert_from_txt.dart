0fc5f93142932a1cd807dbe7660417d123b20ab8
Anxcye <anxcye@outlook.com>
2024-11-28 14:26:56 +0800

    fix: book cover size

--- test/convert/convert_from_txt.dart
+++ test/convert/convert_from_txt.dart
@@ -3,6 +3,6 @@ import 'dart:io';
 import 'package:anx_reader/service/convert_to_epub/txt/convert_from_txt.dart';
 
 void main() {
-  final file = File('assets/local/hlm.txt');
-  final epub = convertFromTxt(file);
+  // final file = File('assets/local/hlm.txt');
+  // final epub = convertFromTxt(file);
 }
