5a278ed529098bf9449b84e285c2371a37f7eefe
Anxcye <axyyu@outlook.com>
2024-12-26 14:00:35 +0800

    fix: windows app crash when open a book #55

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -16,7 +16,7 @@ import package_info_plus
 import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
-import sqflite
+import sqflite_darwin
 import sqlite3_flutter_libs
 import url_launcher_macos
 import wakelock_plus
