0ebbaf79230350f430b62b7d7d796ed4bafe5834
Anxcye <axyyu@outlook.com>
2025-02-04 12:27:00 +0800

    fix: book folder cannot scroll

--- lib/widgets/bookshelf/book_opened_folder.dart
+++ lib/widgets/bookshelf/book_opened_folder.dart
@@ -25,73 +25,63 @@ class _BookOpenedFolderState extends ConsumerState<BookOpenedFolder> {
 
   @override
   Widget build(BuildContext context) {
-    return Dialog(
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              GridView.builder(
-                  shrinkWrap: true,
-                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                    maxCrossAxisExtent: 110,
-                    childAspectRatio: 1 / 1.9,
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 10,
-                  ),
-                  itemCount: books.length,
-                  itemBuilder: (context, index) => Stack(
-                        children: [
-                          BookItem(book: books[index]),
-                          isEditing
-                              ? Positioned(
-                                  right: 0,
-                                  top: 0,
-                                  child: IconButton(
-                                    onPressed: () {
-                                      ref
-                                          .read(bookListProvider.notifier)
-                                          .removeFromGroup(books[index]);
-                                      books.removeAt(index);
-                                      if (books.isEmpty) {
-                                        Navigator.pop(context);
-                                      }
-                                      setState(() {});
-                                    },
-                                    icon: const Icon(
-                                      Icons.remove_circle,
-                                      size: 30,
-                                      color: Colors.red,
-                                    ),
-                                  ),
-                                )
-                              : Container(),
-                        ],
-                      )),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  TextButton(
-                      onPressed: () {
-                        ref.read(bookListProvider.notifier).dissolveGroup(books);
-                        Navigator.pop(context);
-                      },
-                      child: Text(L10n.of(context).common_dissolve)),
-                  TextButton(
-                      onPressed: () {
-                        isEditing = !isEditing;
-                        setState(() {});
-                      },
-                      child: Text(isEditing
-                          ? L10n.of(context).common_cancel
-                          : L10n.of(context).common_edit)),
-                ],
-              ),
-            ],
-          ),
-        ),
+    return AlertDialog(
+      content: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.7,
+        child: GridView.builder(
+            shrinkWrap: true,
+            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 100,
+              childAspectRatio: 1 / 1.9,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+            ),
+            itemCount: books.length,
+            itemBuilder: (context, index) => Stack(
+                  children: [
+                    BookItem(book: books[index]),
+                    isEditing
+                        ? Positioned(
+                            right: 0,
+                            top: 0,
+                            child: IconButton(
+                              onPressed: () {
+                                ref
+                                    .read(bookListProvider.notifier)
+                                    .removeFromGroup(books[index]);
+                                books.removeAt(index);
+                                if (books.isEmpty) {
+                                  Navigator.pop(context);
+                                }
+                                setState(() {});
+                              },
+                              icon: const Icon(
+                                Icons.remove_circle,
+                                size: 30,
+                                color: Colors.red,
+                              ),
+                            ),
+                          )
+                        : Container(),
+                  ],
+                )),
       ),
+      actions: [
+        TextButton(
+            onPressed: () {
+              ref.read(bookListProvider.notifier).dissolveGroup(books);
+              Navigator.pop(context);
+            },
+            child: Text(L10n.of(context).common_dissolve)),
+        TextButton(
+            onPressed: () {
+              isEditing = !isEditing;
+              setState(() {});
+            },
+            child: Text(isEditing
+                ? L10n.of(context).common_cancel
+                : L10n.of(context).common_edit)),
+      ],
     );
   }
 }
