89d339ebdf3c12e966032a2848c5a7fcf5dc5768
Anxcye <axyyu@outlook.com>
2025-02-12 23:03:51 +0800

    perf: parse txt content with regex

--- lib/service/convert_to_epub/txt/convert_from_txt.dart
+++ lib/service/convert_to_epub/txt/convert_from_txt.dart
@@ -1,16 +1,18 @@
 import 'dart:convert';
 import 'dart:io';
-import 'dart:isolate';
 
 import 'package:anx_reader/service/convert_to_epub/create_epub.dart';
+import 'package:anx_reader/service/convert_to_epub/section.dart';
 import 'package:anx_reader/utils/log/common.dart';
 import 'package:fast_gbk/fast_gbk.dart';
 
-List<String> readFileWithEncoding(File file) {
-  bool checkGarbled(List<String> lines) {
-    final garbledPattern = RegExp(r'[¡-ÿ]{2,}|[²-º]|Õ|Ê|Ç|³|¾|Ð|Ó|Î|Á|É');
+String readFileWithEncoding(File file) {
+  bool checkGarbled(String content) {
+    final garbledPattern = RegExp(r'[¡-ÿ]{2,}|[²-º]|Õ|Ê|Ç|³|¾|Ð|Ó|Î|Á|É|�');
 
-    final sampleLines = lines.take(10);
+    final lines = content.split('\n');
+
+    final sampleLines = lines.take(lines.length > 10 ? 10 : lines.length);
     return sampleLines.any((line) => garbledPattern.hasMatch(line));
   }
 
@@ -22,10 +24,10 @@ List<String> readFileWithEncoding(File file) {
   for (final encoding in encodings) {
     try {
       AnxLog.info('Convert: Reading file with encoding: ${encoding.name}');
-      final lines = file.readAsLinesSync(encoding: encoding);
+      final content = file.readAsStringSync(encoding: encoding);
 
-      if (!checkGarbled(lines)) {
-        return lines;
+      if (!checkGarbled(content)) {
+        return content;
       }
       AnxLog.info('Convert: Detected garbled text, trying next encoding');
     } catch (e) {
@@ -36,69 +38,11 @@ List<String> readFileWithEncoding(File file) {
   try {
     AnxLog.info('Convert: Reading file with encoding: gbk');
     final content = gbk.decode(file.readAsBytesSync());
-    return content.split('\n');
+    return content;
   } catch (e) {
     AnxLog.severe('Convert: Failed to read file with encoding');
-    return [];
+    return '';
   }
-}
-
-Future<List<String>> processChunkInIsolate(Map<String, dynamic> params) async {
-  final List<String> lines = params['lines'];
-  final chapters = <String>[];
-  var orientation = params['orientation'];
-  var currentChapter = StringBuffer();
-
-  final prologuePattern = RegExp(r'^\s*(楔子|序章|序言|序|引子).*');
-  final volumePattern = RegExp(
-      r'^\s*(?:[第][0123456789ⅠI一二三四五六七八九十零序〇百千两]*[卷]|[卷][0123456789ⅠI一二三四五六七八九十零序〇百千两]*[ ]|(Vol(?:ume)?\.?|Book)\s*[0123456789ⅠI]*\s*[ ]).*');
-  final chapterPattern = RegExp(
-      r'^\s*(?:[第][0123456789ⅠI一二三四五六七八九十零序〇百千两]*[章]|(Chapter|Ch\.?)\s*[0123456789ⅠI]*\s*[ ]).*');
-
-  void addChapter() {
-    if (currentChapter.isNotEmpty) {
-      chapters.add(currentChapter.toString());
-      currentChapter.clear();
-    }
-  }
-
-  for (var line in lines) {
-    line = line.trim();
-    if (line.isEmpty) continue;
-
-    if (!orientation) {
-      if (line.startsWith('简介') || line.startsWith('内容简介')) {
-        addChapter();
-        currentChapter.writeln('## $line');
-        continue;
-      }
-
-      if (prologuePattern.hasMatch(line)) {
-        addChapter();
-        currentChapter.writeln('## $line');
-        continue;
-      }
-    }
-
-    if (volumePattern.hasMatch(line)) {
-      orientation = true;
-      addChapter();
-      currentChapter.writeln('# $line');
-      continue;
-    }
-
-    if (chapterPattern.hasMatch(line)) {
-      orientation = true;
-      addChapter();
-      currentChapter.writeln('## $line');
-      continue;
-    }
-
-    currentChapter.writeln(line);
-  }
-
-  addChapter();
-  return chapters;
 }
 
 Future<File> convertFromTxt(File file) async {
@@ -113,41 +57,52 @@ Future<File> convertFromTxt(File file) async {
   AnxLog.info('convert from txt. title: $titleString, author: $authorString');
 
   // read file
-  final lines = readFileWithEncoding(file);
+  String content = readFileWithEncoding(file);
 
-  // split file to chunks
-  final int chunkSize = (lines.length / Platform.numberOfProcessors).ceil();
-  final chunks = <List<String>>[];
+  // content = content.replaceAll(RegExp(r'(\n*|^)(\s|　)+'), '\n');
 
-  for (var i = 0; i < lines.length; i += chunkSize) {
-    chunks.add(lines.sublist(
-        i, i + chunkSize > lines.length ? lines.length : i + chunkSize));
+  AnxLog.info('convert from txt. content: ${content.length}');
+
+  final patternStr = RegExp(
+    r'^(.* +|　*)?([第][一二三四五六七八九十零〇百千万两0123456789]+[章卷]|[卷][一二三四五六七八九十零〇百千万两0123456789]+|[Cc]hap(?:ter)\.?|[Vv]ol(?:ume)?\.?|[Bb]ook|[Bb]k)( +.*)?$',
+    multiLine: true,
+  );
+
+  final matches = patternStr.allMatches(content).toList();
+  final sections = <Section>[];
+
+  AnxLog.info('matches: ${matches.length}');
+
+  if (matches.isEmpty) {
+    return createEpub(titleString, authorString, ['#$content}']);
   }
 
-  // process each chunk in parallel
-  final futures = chunks.map((chunk) {
-    return Isolate.run(() => processChunkInIsolate({
-          'lines': chunk,
-          'orientation': false,
-        }));
+  if (matches.first.start > 0) {
+    sections.add(Section('', content.substring(0, matches.first.start), 0));
+  }
+
+  for (int i = 0; i < matches.length; i++) {
+    final match = matches[i];
+    final title = match.group(0)!;
+
+    final startPos = match.start;
+    final endPos =
+        i < matches.length - 1 ? matches[i + 1].start : content.length;
+
+    final fullContent = content.substring(startPos, endPos);
+    final contentWithoutTitle = fullContent.substring(title.length).trim();
+
+    final volumeKeyword = ['卷', 'Book', 'bk', 'Vol'];
+    final level =
+        volumeKeyword.any((keyword) => title.contains(keyword)) ? 1 : 2;
+
+    sections.add(Section(title.trim(), contentWithoutTitle.trim(), level));
+  }
+
+  final volumes = sections.map((section) {
+    return section.toString();
   }).toList();
 
-  final results = await Future.wait(futures);
-  List chapters = results.expand((x) => x).toList();
-  List<String> chaptersAfterRearrange = [];
-
-  // if some String not begin with '#' or '##',  concat them after previous one
-  for (var chapter in chapters) {
-    if (chapter.startsWith('#') || chapter.startsWith('##')) {
-      chaptersAfterRearrange.add(chapter);
-    } else {
-      chaptersAfterRearrange.isNotEmpty
-          ? chaptersAfterRearrange.last +=chapter
-          : chaptersAfterRearrange.add(chapter);
-    }
-  }
-
-  final epubFile =
-      await createEpub(titleString, authorString, chaptersAfterRearrange);
+  final epubFile = await createEpub(titleString, authorString, volumes);
   return epubFile;
 }
