cf99baf886bf7dbcb0e74794fead19063248227f
Anxcye <axyyu@outlook.com>
2025-02-16 20:02:29 +0800

    feat: add donate button

---
+++ lib/widgets/settings/show_donate_dialog.dart
@@ -0,0 +1,24 @@
+import 'package:anx_reader/l10n/generated/L10n.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void showDonateDialog(BuildContext context) {
+  SmartDialog.show(
+    builder: (context) => AlertDialog(
+      title: Text(L10n.of(context).app_donate),
+      content: Text(L10n.of(context).app_donate_tips),
+      actions: [
+        TextButton(
+          onPressed: () {
+            launchUrl(
+              Uri.parse('https://anxcye.com/home/7'),
+              mode: LaunchMode.externalApplication,
+            );
+          },
+          child: Text(L10n.of(context).app_donate),
+        ),
+      ],
+    ),
+  );
+}
