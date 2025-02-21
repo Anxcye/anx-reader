475f30c9c71a3d177978ac9d2ef8c85f5e586d3e
Anxcye <anxcye@outlook.com>
2025-02-21 20:01:05 +0800

    feat: add quick webdav switch on settings page

---
+++ lib/widgets/settings/webdav_switch.dart
@@ -0,0 +1,31 @@
+import 'package:anx_reader/config/shared_preference_provider.dart';
+import 'package:anx_reader/l10n/generated/L10n.dart';
+import 'package:anx_reader/providers/anx_webdav.dart';
+import 'package:anx_reader/utils/webdav/test_webdav.dart';
+import 'package:anx_reader/widgets/settings/settings_tile.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+AbstractSettingsTile webdavSwitch(BuildContext context, Function setState, WidgetRef ref) {
+  return SettingsTile.switchTile(
+    leading: const Icon(Icons.cached),
+    initialValue: Prefs().webdavStatus,
+    onToggle: (bool value) async {
+      setState(() {
+        Prefs().saveWebdavStatus(value);
+      });
+      if (value) {
+        bool result = await testEnableWebdav();
+        if (!result) {
+          setState(() {
+            Prefs().saveWebdavStatus(!value);
+          });
+        } else {
+          AnxWebdav().init();
+          chooseDirection(ref);
+        }
+      }
+    },
+    title: Text(L10n.of(context).settings_sync_enable_webdav),
+  );
+}
