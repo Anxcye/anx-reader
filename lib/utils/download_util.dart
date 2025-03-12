adaf93ebc64d7a0fd57d3ef34870b1d17ec3c718
anxcye <anxcye@outlook.com>
2025-03-12 21:10:17 +0800

    feat: save image on ios

--- lib/utils/download_util.dart
+++ lib/utils/download_util.dart
@@ -10,7 +10,6 @@ import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:saver_gallery/saver_gallery.dart';
 
-/// From https://github.com/guozhigq/pilipala which is GPL-3.0 Licensed
 class DownloadUtil {
   static Future<bool> requestStoragePer() async {
     await Permission.storage.request();
@@ -70,27 +69,14 @@ class DownloadUtil {
     }
   }
 
-  static Future<bool> androidImgSaver(
-      Uint8List img, String extension, String name) async {
+  static Future<bool> saveImg(
+    Uint8List img,
+    String extension,
+    String name,
+  ) async {
     try {
-      if (defaultTargetPlatform != TargetPlatform.android) return true;
-      final deviceInfoPlugin = DeviceInfoPlugin();
-      final deviceInfo = await deviceInfoPlugin.androidInfo;
-      final sdkInt = deviceInfo.version.sdkInt;
-      AnxLog.info('sdkInt: $sdkInt');
-
-      if (sdkInt > 33) {
-        if (!await requestPhotoPer()) {
-          return false;
-        }
-      } else {
-        if (!await requestStoragePer()) {
-          return false;
-        }
-      }
       SmartDialog.showLoading(
           msg: L10n.of(navigatorKey.currentContext!).common_saving);
-
 
       final SaveResult result = await SaverGallery.saveImage(
         img,
@@ -113,9 +99,42 @@ class DownloadUtil {
     }
   }
 
+  static Future<bool> androidImgSaver(
+    Uint8List img,
+    String extension,
+    String name,
+  ) async {
+    try {
+      if (defaultTargetPlatform != TargetPlatform.android) return true;
+      final deviceInfoPlugin = DeviceInfoPlugin();
+      final deviceInfo = await deviceInfoPlugin.androidInfo;
+      final sdkInt = deviceInfo.version.sdkInt;
+      AnxLog.info('sdkInt: $sdkInt');
+
+      if (sdkInt > 33) {
+        if (!await requestPhotoPer()) {
+          return false;
+        }
+      } else {
+        if (!await requestStoragePer()) {
+          return false;
+        }
+      }
+      return await saveImg(img, extension, name);
+    } catch (err) {
+      SmartDialog.dismiss();
+      AnxToast.show(L10n.of(navigatorKey.currentContext!).common_failed);
+      AnxLog.severe("saveImage: saveImage error: $err");
+      return true;
+    }
+  }
+
   // windows just save the image to the download path
   static Future<bool> windowsImgSaver(
-      Uint8List img, String extension, String name) async {
+    Uint8List img,
+    String extension,
+    String name,
+  ) async {
     String? path = await fileSaver(
       bytes: img,
       fileName: '$name.$extension',
@@ -129,6 +148,14 @@ class DownloadUtil {
     return true;
   }
 
+  static Future<bool> iosImgSaver(
+    Uint8List img,
+    String extension,
+    String name,
+  ) async {
+    return await saveImg(img, extension, name);
+  }
+
   static Future<bool> downloadImg(
     Uint8List img,
     String extension,
@@ -140,11 +167,12 @@ class DownloadUtil {
       case TargetPlatform.android:
         return await androidImgSaver(img, extension, picName);
       case TargetPlatform.windows:
-      case TargetPlatform.iOS:
       case TargetPlatform.macOS:
         return await windowsImgSaver(img, extension, picName);
+      case TargetPlatform.iOS:
+        return await iosImgSaver(img, extension, name);
       default:
-      throw Exception('Unsupported platform');
+        throw Exception('Unsupported platform');
     }
   }
 }
