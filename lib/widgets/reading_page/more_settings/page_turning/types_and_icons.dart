d2ae0c23d1bd86d92b1ac5e68085993569d508f2
anxcye <anxcye@outlook.com>
2025-03-31 15:27:05 +0800

    feat: add a new page turn method

--- lib/widgets/reading_page/more_settings/page_turning/types_and_icons.dart
+++ lib/widgets/reading_page/more_settings/page_turning/types_and_icons.dart
@@ -53,5 +53,19 @@ List<PageTurningType> type4 = [
 // next, prev, menu
 List<int> icon4 = [5, 3, 1];
 
-List<List<PageTurningType>> pageTurningTypes = [type1, type2, type3, type4];
-List<List<int>> pageTurningIcons = [icon1, icon2, icon3, icon4];
+List<PageTurningType> type5 = [
+  PageTurningType.next,
+  PageTurningType.menu,
+  PageTurningType.next,
+  PageTurningType.next,
+  PageTurningType.prev,
+  PageTurningType.next,
+  PageTurningType.next,
+  PageTurningType.next,
+  PageTurningType.next
+];
+// next, prev, menu from 0
+List<int> icon5 = [7, 4, 1];
+
+List<List<PageTurningType>> pageTurningTypes = [type1, type2, type3, type4, type5];
+List<List<int>> pageTurningIcons = [icon1, icon2, icon3, icon4, icon5];
