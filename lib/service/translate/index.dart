3c8bcdae6831bd35812603265fcab8645998cd7c
Anxcye <axyyu@outlook.com>
2025-05-06 14:05:35 +0800

    feat: add translate configuration

--- lib/service/translate/index.dart
+++ lib/service/translate/index.dart
@@ -18,8 +18,56 @@ TranslateService getTranslateService(String name) {
   return TranslateService.values.firstWhere((e) => e.name == name);
 }
 
+enum ConfigItemType {
+  text('text input'),
+  password('password input'),
+  number('number input'),
+  select('select'),
+  radio('radio'),
+  checkbox('checkbox'),
+  toggle('toggle');
+
+  const ConfigItemType(this.label);
+  final String label;
+}
+
+class ConfigItem {
+  final String key;
+  final String label;
+  final String? description;
+  final ConfigItemType type;
+  final dynamic defaultValue;
+  final List<Map<String, dynamic>>? options; // 用于select, radio等类型的选项
+
+  ConfigItem({
+    required this.key,
+    required this.label,
+    this.description,
+    required this.type,
+    this.defaultValue,
+    this.options,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'key': key,
+      'label': label,
+      'description': description,
+      'type': type.name,
+      'defaultValue': defaultValue,
+      'options': options,
+    };
+  }
+}
+
 abstract class TranslateServiceProvider {
   Stream<String> translate(String text, LangListEnum from, LangListEnum to);
+
+  List<ConfigItem> getConfigItems();
+
+  Map<String, dynamic> getConfig();
+
+  void saveConfig(Map<String, dynamic> config);
 }
 
 class TranslateFactory {
@@ -39,4 +87,17 @@ Stream<String> translateText(String text, {TranslateService? service}) {
   final to = Prefs().translateTo;
 
   return TranslateFactory.getProvider(service).translate(text, from, to);
+}
+
+List<ConfigItem> getTranslateServiceConfigItems(TranslateService service) {
+  return TranslateFactory.getProvider(service).getConfigItems();
+}
+
+Map<String, dynamic> getTranslateServiceConfig(TranslateService service) {
+  return TranslateFactory.getProvider(service).getConfig();
+}
+
+void saveTranslateServiceConfig(
+    TranslateService service, Map<String, dynamic> config) {
+  return TranslateFactory.getProvider(service).saveConfig(config);
 }
