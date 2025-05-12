a974699dd31584b8a31fc569acbf939bf7210fad
Anxcye <anxcye@outlook.com>
2025-05-12 16:45:12 +0800

    fix: show asset bgimg

--- lib/models/bgimg.dart
+++ lib/models/bgimg.dart
@@ -1,4 +1,6 @@
+import 'package:anx_reader/enums/bgimg_alignment.dart';
 import 'package:anx_reader/enums/bgimg_type.dart';
+import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'bgimg.freezed.dart';
@@ -9,11 +11,16 @@ abstract class BgimgModel with _$BgimgModel {
   const factory BgimgModel({
     required BgimgType type,
     required String path,
+    required BgimgAlignment alignment,
   }) = _BgimgModel;
 
   factory BgimgModel.fromJson(Map<String, dynamic> json) =>
       _$BgimgModelFromJson(json);
 
   const BgimgModel._();
+
   String get url => type == BgimgType.assets ? 'assets/$path' : path;
+
+  ImageProvider get image =>
+      type == BgimgType.assets ? AssetImage(path) : NetworkImage(url);
 }
