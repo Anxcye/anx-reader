2f130a992ff7e0acd0ad6fa7bf4054c9f9b4667e
Anxcye <anxcye@outlook.com>
2025-05-12 17:18:49 +0800

    feat: import local bgimg

--- lib/providers/bgimg.dart
+++ lib/providers/bgimg.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+
 import 'package:anx_reader/enums/bgimg_alignment.dart';
 import 'package:anx_reader/enums/bgimg_type.dart';
 import 'package:anx_reader/models/bgimg.dart';
+import 'package:anx_reader/utils/get_path/get_base_path.dart';
+import 'package:anx_reader/utils/log/common.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'bgimg.g.dart';
@@ -45,6 +50,45 @@ class Bgimg extends _$Bgimg {
   }
 
   List<BgimgModel> listLocal() {
-    return [];
+    final bgimgDir = getBgimgDir();
+    if (!bgimgDir.existsSync()) {
+      return [];
+    }
+    return bgimgDir.listSync().map((e) {
+      return BgimgModel(
+          type: BgimgType.localFile,
+          path: e.path.split(Platform.pathSeparator).last,
+          alignment: BgimgAlignment.center);
+    }).toList();
+  }
+
+  void deleteBgimg(BgimgModel bgimgModel) {
+    final path = getBgimgDir().path + Platform.pathSeparator + bgimgModel.path;
+    if (File(path).existsSync()) {
+      File(path).deleteSync();
+      ref.invalidateSelf();
+    }
+  }
+
+  Future<void> importImg() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+      allowMultiple: false,
+    );
+
+    if (result == null) {
+      return;
+    }
+
+    File image = File(result.files.single.path!);
+
+    AnxLog.info('BookDetail: Image path: ${image.path}');
+
+    final extName = image.path.split('.').last;
+    final newName = '${DateTime.now().millisecondsSinceEpoch}.$extName';
+    final newPath = '${getBgimgDir().path}${Platform.pathSeparator}$newName';
+
+    await image.copy(newPath);
+    ref.invalidateSelf();
   }
 }
