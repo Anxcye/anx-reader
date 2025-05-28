c50210c6d33f39b82d4756bdadf3a543223e6a4d
Anxcye <anxcye@outlook.com>
2025-05-28 15:03:53 +0800

    feat: create bookmark model

---
+++ lib/models/bookmark.dart
@@ -0,0 +1,37 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'bookmark.freezed.dart';
+part 'bookmark.g.dart';
+
+@freezed
+abstract class BookmarkModel with _$BookmarkModel {
+  const factory BookmarkModel({
+    int? id,
+    required int bookId,
+    required String content,
+    required String cfi,
+    required String chapter,
+    DateTime? createTime,
+    required DateTime updateTime,
+  }) = _BookmarkModel;
+
+  factory BookmarkModel.fromJson(Map<String, dynamic> json) =>
+      _$BookmarkModelFromJson(json);
+}
+
+extension BookmarkModelExtension on BookmarkModel {
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'book_id': bookId,
+      'content': content,
+      'cfi': cfi,
+      'chapter': chapter,
+      'type': 'bookmark',
+      'color': 'None',
+      'reader_note': 'None',
+      'create_time': createTime?.toIso8601String(),
+      'update_time': updateTime.toIso8601String(),
+    };
+  }
+}
