427520069e705fc91455c3df5b55e8d4c77090d4
Anxcye <axyyu@outlook.com>
2025-05-31 15:22:22 +0800

    Feat: Support displaying page numbers in the table of contents

--- lib/models/toc_item.dart
+++ lib/models/toc_item.dart
@@ -2,16 +2,32 @@ class TocItem {
   final String id;
   final String href;
   final String label;
+  final int level;
+  final int startPage;
+  final double startPercentage;
   final List<TocItem> subitems;
 
-  TocItem({required this.id, required this.href, required this.label, required this.subitems});
+  TocItem(
+      {required this.id,
+      required this.href,
+      required this.label,
+      required this.subitems,
+      required this.level,
+      required this.startPage,
+      required this.startPercentage});
 
   factory TocItem.fromJson(Map<String, dynamic> json) {
     return TocItem(
       id: json['id'].toString(),
       href: json['href'],
       label: json['label'],
-      subitems: json['subitems'] == null ? [] : (json['subitems'] as List).map((i) => TocItem.fromJson(i)).toList(),
+      startPage: json['startPage'] ?? 0,
+      startPercentage: (json['startPercentage'] ?? 0.0).toDouble(),
+      level: json['level'] ?? 0,
+      subitems: json['subitems'] == null
+          ? []
+          : (json['subitems'] as List).map((i) => TocItem.fromJson(i)).toList(),
+
     );
   }
 }
