4bae28036ce9b319bf71b3f8d933fc7a60c8373f
Anxcye <anxcye@outlook.com>
2025-09-14 16:39:18 +0800

    fix: update locale retrieval to use toLanguageTag for better language code handling

--- lib/utils/get_current_language_code.dart
+++ lib/utils/get_current_language_code.dart
@@ -2,11 +2,15 @@ import 'dart:io';
 import 'package:anx_reader/config/shared_preference_provider.dart';
 
 String getCurrentLanguageCode() {
-  String? locale = Prefs().locale?.languageCode;
+  String? locale = Prefs().locale?.toLanguageTag();
 
-  if (locale == null) {
-    return Platform.localeName.split('_')[0];
-  }
-  return locale;
+    locale ??= Platform.localeName;
+  
+    if (locale.startsWith('zh_Hans')) {
+      return 'zh-CN';
+    } else if (locale.startsWith('zh_Hant')) {
+      return 'zh-TW';
+    } else {
+      return Platform.localeName.split('_')[0];
+    }
 }
-
